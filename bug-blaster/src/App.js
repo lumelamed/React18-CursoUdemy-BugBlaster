ca01a8af92c1b6610e8e6c3640146c709788bb42
MELAMED, LUCILA (Servicio Externo en YPF) <SE4412@grupo.ypf.com>
2024-10-22 23:09:37 -0300

    default app with custom styles

---
+++ bug-blaster/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Bug Blaster</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
