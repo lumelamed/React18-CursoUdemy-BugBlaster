e51c50ca214635a9c72f78d7c8d435648127273d
MELAMED, LUCILA (Servicio Externo en YPF) <SE4412@grupo.ypf.com>
2024-10-30 00:38:02 -0300

    ticket item y list, edit ticket y sorting

--- src/App.js
+++ src/App.js
@@ -3,17 +3,48 @@ import "./styles.css";
 import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
 import tickerReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
-  const initialState = { tickets: [] };
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to Low",
+  };
 
   const [state, dispatch] = useReducer(tickerReducer, initialState);
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm dispatch={dispatch}></TicketForm>
+        <TicketForm
+          dispatch={dispatch}
+          editingTicket={state.editingTicket}
+        ></TicketForm>
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <select
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+            >
+              <option value="High to Low">High to Low</option>
+              <option value="Low to High">Low to High</option>
+            </select>
+
+            <TicketList
+              tickets={sortedTickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
       </div>
     </div>
   );
